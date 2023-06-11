094a020da8540bef622b22473d13a24875585745
JoanaMas <joana.mastianica@gmail.com>
2023-06-11 20:39:10 +0300

    19: Mobile menu UI adjustments

--- src/components/navbar/_mobileLinks/MobileLinks.tsx
+++ src/components/navbar/_mobileLinks/MobileLinks.tsx
@@ -1,18 +1,30 @@
-import React, { FC, ReactElement } from "react";
+import React, { FC, ReactElement, useState, useEffect } from "react";
+import { useWindowSize } from "@react-hook/window-size";
 // Components
 import NavLink from "../_navLink/NavLink";
 // Style
 import styles from "./mobileLinks.module.scss";
 
 const MobileLinks: FC = (): ReactElement => {
+
+    // WINDO WIDTH ON RESIZE LOGIC
+    const [windowWidth] = useWindowSize();
+    const [screenWidth, setScreenWidth] = useState(0);
+  
+    useEffect(() => {
+      setScreenWidth(windowWidth);
+    }, [windowWidth]);
+
   return (
-    <div className={styles.mobileMenuContainer}>
-    <div className={styles.links}>
-      <NavLink route="/">Chrome Extention</NavLink>
-      <NavLink route="#">Price Comparison</NavLink>
-      <NavLink route="#">Blog</NavLink>
-    </div>
-  </div>
+      <div className={styles.mobileMenuContainer}>
+        <div className={styles.linksContainer}>
+        <div className={styles.links}>
+          <NavLink route={screenWidth >= 800 ? "/" : "#"}>Chrome Extention</NavLink>
+          <NavLink route="#">Price Comparison</NavLink>
+          <NavLink route="#">Blog</NavLink>
+        </div>
+        </div>
+      </div>
   );
 };
 
